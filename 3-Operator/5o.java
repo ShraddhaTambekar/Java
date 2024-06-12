bcf74eb4f18d1c6e6c8b863a76ec9232d196b39c
Shraddha Tambekar <github108108@gmail.com>
2024-06-12 17:39:38 +0530

    Add files via upload

---
+++ 3-Operator/5o.java
@@ -0,0 +1,13 @@
+class Demo{
+    public static void main(String [] args){
+        
+        int x = 10;
+        int y = 20;
+
+        if(true)                           
+            System.out.println("Hello");
+        else
+            System.out.println("Hi");  
+
+    }
+}
