d485ea5d57a9b2d0adaf9c78be03444856609d7b
Shraddha Tambekar <github108108@gmail.com>
2024-06-12 17:42:48 +0530

    Add files via upload

---
+++ 3-Operator/3o.java
@@ -0,0 +1,14 @@
+class Demo{                                //Relational Operator
+    public static void main(String [] args){
+        
+        int x = 10;
+        int y = 20;
+                                   
+        System.out.println(x<y);   //true     Relational operator gives true/false value only
+        System.out.println(x>y);   //false
+        System.out.println(x<=y);  //true
+        System.out.println(x>=y);  //false
+        System.out.println(x==y);  //false
+        System.out.println(x!=y);  //true
+    }
+}
