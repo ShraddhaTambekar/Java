4a9bc2294346b14af284c7cda41450c189085eee
Shraddha Tambekar <github108108@gmail.com>
2024-06-12 17:29:22 +0530

    Add files via upload
    operators code

---
+++ 3-Operator/1o.java
@@ -0,0 +1,14 @@
+class Demo{                                 //Arithematic Operator
+    public static void main(String [] args){
+        
+        int x = 10;
+        int y = 20;
+                                   
+        System.out.println(x+y);   //30
+        System.out.println(x-y);   //-10
+        System.out.println(x*y);   //200
+        System.out.println(x/y);   //0 Any arithematic operation on numbers gives int output therefor take integer part of 0.5
+        System.out.println(x%y);   //10
+
+    }
+}
